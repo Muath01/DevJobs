c283b21d4d0df9a7fc70dbf75d9a7f091814ea3e
Muath Khalifa <muath.khalifa@yahoo.com>
2023-08-09 19:26:19 +0200

    check gitpages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import Registration from "./components/Registration.tsx";
 import SavedJobs from "./components/SavedJobs.tsx";
 
 function App() {
+  console.log("on app.tsx");
   return (
     <>
       <div className="w-full h-[91%] absolute ">
